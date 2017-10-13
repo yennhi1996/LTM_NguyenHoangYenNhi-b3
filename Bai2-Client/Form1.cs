2492cc13de629ab3ae3f001939cbd2315497eb3a
nataurus9x <nataurus9x@gmail.com>
2017-10-13 20:36:19 +0700

    Buoi3

---
+++ Bai2-Client/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+namespace Bai2_Client
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        IPEndPoint ipserver;
+        EndPoint remote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ipserver = new IPEndPoint(IPAddress.Parse("127.0.0.0"), 9050);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            remote = (EndPoint)ipserver;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Keo";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, ipserver);
+
+
+            byte[] rec = new byte[25];
+            remote = (EndPoint)ipserver;
+            client.ReceiveFrom(rec, ref remote);
+
+            textBox1.Text = Encoding.ASCII.GetString(rec);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text = "Bua";
+            int a = 1;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, ipserver);
+
+
+            byte[] rec = new byte[25];
+            remote = (EndPoint)ipserver;
+            client.ReceiveFrom(rec, ref remote);
+
+            textBox1.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text = "Bao";
+            int a = 2;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, ipserver);
+
+
+            byte[] rec = new byte[25];
+            remote = (EndPoint)ipserver;
+            client.ReceiveFrom(rec, ref remote);
+
+            textBox1.Text = Encoding.ASCII.GetString(rec);
+        }
+    }
+}
---
+++ Bai2-Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Bai2_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(ipep);
+
+            Console.WriteLine("Waiting for client...:");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref Remote);
+            int ClientChoosen = BitConverter.ToInt32(data, 0);
+
+            Random rand = new Random();
+            int ServerChoosen = rand.Next(0, 2);
+            
+            if(ClientChoosen == ServerChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Draw");
+                server.SendTo(send, Remote);
+            }
+            if (ClientChoosen > ServerChoosen)
+            {
+                if (ServerChoosen == 0 && ClientChoosen == 2)
+                {
+                    byte[] send = Encoding.ASCII.GetBytes("Lose");
+                    server.SendTo(send, Remote);
+                }
+                else
+                {
+                    byte[] send = Encoding.ASCII.GetBytes("Win");
+                    server.SendTo(send, Remote);
+                }
+            }
+            if(ClientChoosen < ServerChoosen)
+            {
+                if(ServerChoosen == 2 && ClientChoosen == 0)
+                {
+                    byte[] send = Encoding.ASCII.GetBytes("Win");
+                    server.SendTo(send, Remote);
+                }
+                else
+                {
+                    byte[] send = Encoding.ASCII.GetBytes("Lose");
+                    server.SendTo(send, Remote);
+                }
+            }
+        }
+    }
+}
